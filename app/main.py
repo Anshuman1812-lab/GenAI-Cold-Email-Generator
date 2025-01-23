3566c05dfd4391e8895c7ed78ee841ad9d231b4e
Anshuman Singh <anshumans.work96@gmail.com>
2025-01-22 22:58:15 -0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ from portfolio import Portfolio
 from utils import clean_text
 
 def create_streamlit_app(llm, portfolio, clean_text):
-    st.title("📧 Cold Email generator")
+    st.title("📧 Impact Mailer")
     url_input = st.text_input("Enter a URL: ", value="https://jobs.nike.com/job/R-45889?from=job%20search%20funnel")
     submit_button = st.button("Submit")
 
@@ -26,5 +26,5 @@ def create_streamlit_app(llm, portfolio, clean_text):
 if __name__ == "__main__":
     chain = Chain()
     portfolio = Portfolio()
-    st.set_page_config(layout="wide", page_title="Cold Email Generator", page_icon="📧")
+    st.set_page_config(layout="wide", page_title="Impact Mailer", page_icon="📧")
     create_streamlit_app(chain, portfolio, clean_text)
